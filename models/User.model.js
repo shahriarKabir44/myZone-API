efc084dd75fb35fa7dd8628dbd07a9d5d9e57ad4
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2022-10-07 00:28:18 +0600

    added search by interest api

--- models/User.model.js
+++ models/User.model.js
@@ -3,23 +3,26 @@ const QueryBuilder = require('../utils/QueryBuilder')
 const defaultCoverPhoto = "https://www.al.com/resizer/ILBcdq1ksZC39_8hhnJ_HXsP9j0=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/NSDL77J3KJFZXCK3MFWAV7HMUE.JPG"
 
 module.exports = class User {
-    static async searchUsersByInterest({ query }) {
+    static async searchUsersByInterest({ query, userId }) {
         return Promisify({
-            sql: `select Id, name, email, profileImage
+            sql: `select Id, name, email, profileImage,
+            (select friendship_type from friendship
+                where friend1 = ? and friend2 = Id
+                ) as friendshipType
                 from user where Id in (select userId from 
                     user_interests where interestName like ?);`,
-            values: [`%${query}%`]
+            values: [userId, `%${query}%`]
         })
     }
     static async filterUsers({ query, currentUserId, pageNumber }) {
         return Promisify({
             sql: `select name,profileImage, email, Id, 
                 (select friendship_type from friendship
-                where friend1 = 19 and friend2 = Id
+                where friend1 = ? and friend2 = Id
                 ) as friendshipType
                 from user where name like 
                 ? or email like ? and Id!=? limit ?,10;`,
-            values: [`%${query}%`, `%${query}%`, currentUserId, pageNumber]
+            values: [currentUserId, `%${query}%`, `%${query}%`, currentUserId, pageNumber]
         })
     }
     static async findById(Id) {
